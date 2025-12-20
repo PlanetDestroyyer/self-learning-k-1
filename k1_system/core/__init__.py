0d876a34486ad9f19689a2b740c7304c659c646d
PlanetDestroyyer <wrieddude@gmail.com>
2025-12-20 16:00:13 +0530

    Fix: Updated __init__.py files to remove deleted module imports
    - k1_system/learning/__init__.py: Only import HybridK1Trainer
    - k1_system/core/__init__.py: Only import TransformerEncoder
    - Removed references to deleted modules

--- k1_system/core/__init__.py
+++ k1_system/core/__init__.py
@@ -2,16 +2,8 @@
 Core components for the Self-Learning K-1 System.
 """
 
-from .agent import Agent
-from .hierarchy import Hierarchy
-from .trust_system import TrustSystem, TrustCache
-from .routing import HierarchicalRouter, RoutingPath
+from .transformer_agent import TransformerEncoder
 
 __all__ = [
-    'Agent',
-    'Hierarchy',
-    'TrustSystem',
-    'TrustCache',
-    'HierarchicalRouter',
-    'RoutingPath'
+    'TransformerEncoder'
 ]
--- k1_system/learning/__init__.py
+++ k1_system/learning/__init__.py
@@ -2,16 +2,8 @@
 Learning mechanisms for the Self-Learning K-1 System.
 """
 
-from .credit_assignment import CreditAssignmentSystem
-from .forward_pass import ForwardPass
-from .weight_update import WeightUpdater, AdaptiveWeightUpdater
-from .hybrid_trainer import HybridK1Trainer, validate_k1_system
+from .hybrid_trainer import HybridK1Trainer
 
 __all__ = [
-    'CreditAssignmentSystem',
-    'ForwardPass',
-    'WeightUpdater',
-    'AdaptiveWeightUpdater',
-    'HybridK1Trainer',
-    'validate_k1_system'
+    'HybridK1Trainer'
 ]
